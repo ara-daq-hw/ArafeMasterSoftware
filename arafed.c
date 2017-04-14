224b08136ece9bc5b7f8a3b86939b19b84ec5d92
Brian Clark <clark.2668@osu.edu>
2017-04-14 14:27:52 -0400

    Forgot to decrement the argc, never broke out of the main loop. Fixed. FX2 connection seems to work.

--- arafed.c
+++ arafed.c
@@ -3,7 +3,7 @@
  * 
  * Available commands:
  * power - change the power settings for the four ARAFE slaves; you must change all four at once
- * init - intializes the connection to the ARAFE master through the I2C port
+ * connect - intializes the connection to the ARAFE master through the I2C port
  * att - change the attenuator settings for any of the four ARAFE slaves
  * 
  * Modeleded after the IceCal stuff by PSA
@@ -33,23 +33,25 @@ int main(int argc, char **argv){
 
 	int auxFd;
 	int retval;
-	
-	argc--; argv++;
-	if(!argc) {exit(1); }
-	
-	
+   
+	argc--; argv++; //decrease the number of parameters the program thinks it has
+        if(!argc) {printf("nothing in argc, quitting now\n"); exit(1); }
+       
+        auxFd = openConnectionToFx2ControlSocket();
+        if(auxFd ==0) {
+	        fprintf(stderr,"Error opening connection to FX2_CONTROL_SOCKET\n");
+	        exit(1);
+        }
+   
+   
 	while(argc){ //okay, loop over the arguments
-		if (strstr(*argv, "init")) { //if the command is to initialize the port
-			auxFd = openConnectionToFx2ControlSocket(); //open a connection to the FX2 control socket
-			if(auxFd ==0){ //check to see if the FX2 connection actually got made
-				fprintf(stderr, "Error opening connection to FX2_CONTROL_SOCKET\n");
-				exit(1);
-			}
+		if (strstr(*argv, "connect")) { //if the command is to initialize the port
 			//here, I'm going to declare that we want the ARAFE Master to be on EX0, the first I2C port when I try to connect to it
 			if ((retval = enableAtriComponents(auxFd, ext_i2c[0]))<0) { //try and enable this port, and throw an error if that fails
 				fprintf(stderr, "Something went wrong with assigning the ARAFE master to EX3\n");
 				exit(1);
 			}
+		        else printf("connection successful\n");
 		}
 		if (strstr(*argv, "power")) { //if the command is to change the power settings
 			//code needs to be added here to do power control
@@ -57,9 +59,12 @@ int main(int argc, char **argv){
 		if (strstr(*argv, "att")) { //if the command is to change the attenuator settings
 			//code needs to be added here to do attenuator control
 		}
-	
+
+	        argc--;
+	        argv++;
 	} //finish looping over the arguments
 
+   
 	close(auxFd);
 	return 0;
 }
