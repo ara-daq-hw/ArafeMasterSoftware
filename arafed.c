410b50ac678c228639e743428f4dc924f7938c97
Brian Clark <clark.2668@osu.edu>
2017-04-19 09:44:52 -0400

    Power function now fully working

--- arafed.c
+++ arafed.c
@@ -30,7 +30,7 @@
 #include "arafed.h"
 #include "arafei2c.h"
 
-int v =1; //variable to control verbosity of the output, because c doesn't natively support booleans...
+int v =0; //variable to control verbosity of the output, because c doesn't natively support booleans...
 //1 = verbose, 0 = not verbose (!0 = true,  0 = false)
 //naughty thing to do with a global variable, but oh well...
 
@@ -73,22 +73,33 @@ int main(int argc, char **argv){
 			}
 			if(v) printf("connection successful, retval is %d\n",retval); //print out a confirmation if verbosity is active
 		
-		
-			if((argv[1] > 1) || (argv[2] > 1) || (argv[3] > 1) || (argv[4] > 1)){
+		   
+		        int arg1 = atoi(argv[4]);
+		        int arg2 = atoi(argv[3]);
+		        int arg3 = atoi(argv[2]);
+		        int arg4 = atoi(argv[1]);
+		   
+		        if(v) {
+			        printf("arg 1 %d\n", arg1);
+		                printf("arg 2 %d\n", arg2);
+		                printf("arg 3 %d\n", arg3);
+		                printf("arg 4 %d\n", arg4);
+			}
+		   
+			if((arg1 > 1) || (arg2 > 1) || (arg3 > 1) || (arg4> 1)){
 				fprintf(stderr, "You can only turn a slave on or off with 0 or 1, nothing more\n"); //throw an error if the user tries to set them to something greater than 1
 				exit(1);
 			}
 			//now to set up the transmission
-			//they need to set all slaves at once, so [xxxx] = [slave3 slave2 slave1 slave0]
+			//they need to set all slaves at once, so [x x x x] = [slave3 slave2 slave1 slave0], with space in between
 			char str[8]; //declare a blank eight bits
-			sprintf(str,"1000%d%d%d%d",argv[1],argv[2],argv[3],argv[4]); //form this up in binary 
+			sprintf(str,"1000%d%d%d%d",arg1,arg2,arg3,arg4); //form this up in binary 
 			int int_ver = (int) strtol(str, NULL, 2); //convert it to a integer number
 			unsigned char value = (unsigned char) int_ver; //convert it to an unsigned character (this is super naive and wrong, bug I want to have something here...)
-			//unsigned char value = 0X84; //teset value to pass to the slave; this is 10001111 cast into hex
 			unsigned char reg = 0x00; //for power control, this is the register that's necessary
 			retval = arafeWriteRegister(auxFd, reg, value);  //actually write to the register
 			
-			if( retval<0){ //write to register 0 (POWERCTL)  with 8F (High bit set and all slaves on)
+			if( retval<0){ //if it fails
 				printf("retval is %d, and setting the power failed\n", retval); //say something useful if it's wrong
 				exit(1); //exit
 			}
